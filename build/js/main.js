99c760c9f729bee36112e60c670a289519575779
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-04-07 12:51:19 +0600

    lesson-2 finished

---
+++ build/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let myName = "Dave";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "john";
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+album = "pricipia";
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(2, 3));
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+let myName: string = "Dave";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = "john";
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+album = "pricipia";
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+console.log(sum(2, 3));
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
